32eca22f82309c5d85440a64d047a2d98d8e6977
Aditya <aditya@rangde.org>
2017-01-30 13:33:31 +0530

    todolist changes done

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 function TodoCtrl($scope) {
 
-    $scope.todos = [{ text: 'Learn angular', done: false },
-        { text: 'build an app', done: false }
-    ]
+    $scope.todos = ['Learn angular', 'build an app']
 
     $scope.addTodo = function() {
-        $scope.todos.push({ text: $scope.formTodoText, done: false })
-        $scope.formTodoText = ''
-
+        $scope.errortext = ""
+        if (!$scope.addItem) {return}
+        if ($scope.todos.indexOf($scope.addItem) == -1) {
+            $scope.todos.push($scope.addItem)
+            localStorage.setItem('todo',JSON.stringify($scope.todos))
+        } else {
+            $scope.errortext = "You have already added this to your list."
+        }
     }
+
+    $scope.removeItem = function (x) {
+        $scope.errortext = ""
+        $scope.todos.splice(x, 1)
+        localStorage.setItem('todo',JSON.stringify($scope.todos))
+    }
+
     $scope.getTotalTodos = function() {
         return $scope.todos.length
     }
